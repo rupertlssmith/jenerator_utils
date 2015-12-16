dc8c9ef2b113de647cbf7b5aeef48d3a28800761
Rupert Smith <github@badgerhouse.freeserve.co.uk>
2015-12-16 14:56:44 +0000

    Improved error handling of the handlbars templates - DropWizard swallows exceptions here...

--- util_dropwizard_0.9/src/main/java/com/thesett/util/views/handlebars/HandlebarsViewRenderer.java
+++ util_dropwizard_0.9/src/main/java/com/thesett/util/views/handlebars/HandlebarsViewRenderer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright The Sett Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.thesett.util.views.handlebars;
 
 import java.io.IOException;
@@ -34,14 +49,17 @@ import io.dropwizard.views.ViewRenderer;
  * <tr><td> Render handlebars templates from views. </td></tr>
  * </table></pre>
  */
-public class HandlebarsViewRenderer implements ViewRenderer {
+public class HandlebarsViewRenderer implements ViewRenderer
+{
     /** Caches template files. */
     private static final Cache<TemplateSource, Template> templateCache = CacheBuilder.newBuilder().build();
 
     /** Caches compiled templates. */
     static final LoadingCache<String, Template> compilationCache =
-        CacheBuilder.newBuilder().build(new CacheLoader<String, Template>() {
-                public Template load(String srcUrl) throws Exception {
+        CacheBuilder.newBuilder().build(new CacheLoader<String, Template>()
+            {
+                public Template load(String srcUrl) throws Exception
+                {
                     return loadTemplate(srcUrl);
                 }
             });
@@ -53,7 +71,8 @@ public class HandlebarsViewRenderer implements ViewRenderer {
     protected static boolean useCache = true;
 
     /** {@inheritDoc} */
-    public void configure(Map<String, String> stringStringMap) {
+    public void configure(Map<String, String> stringStringMap)
+    {
     }
 
     /**
@@ -61,14 +80,18 @@ public class HandlebarsViewRenderer implements ViewRenderer {
      *
      * <p/>Templates ending with ".hbs" are rendered by this.
      */
-    public boolean isRenderable(View view) {
+    public boolean isRenderable(View view)
+    {
         return view.getTemplateName().endsWith(".hbs");
     }
 
     /** {@inheritDoc} */
-    public void render(View view, Locale locale, OutputStream output) throws IOException, WebApplicationException {
-        try(Writer writer = new OutputStreamWriter(output, view.getCharset().or(Charsets.UTF_8))) {
-            if (view instanceof Layout) {
+    public void render(View view, Locale locale, OutputStream output) throws IOException, WebApplicationException
+    {
+        try(Writer writer = new OutputStreamWriter(output, view.getCharset().or(Charsets.UTF_8)))
+        {
+            if (view instanceof Layout)
+            {
                 Writer buffer = new StringWriter();
 
                 Layout layout = (Layout) view;
@@ -80,14 +103,23 @@ public class HandlebarsViewRenderer implements ViewRenderer {
                 Map<String, String> vals = new HashMap<>();
                 vals.put("body", buffer.toString());
                 layoutTemplate.apply(vals, writer);
-            } else {
+            }
+            else
+            {
                 loadTemplate(view.getTemplateName()).apply(view, writer);
             }
+        }
+        catch (IOException | WebApplicationException e)
+        {
+            // The exception handling in dropwizard view renderer is so bad. So rethrowing this as an illegal state
+            // - system exception.
+            throw new IllegalStateException(e);
         }
     }
 
     /** {@inheritDoc} */
-    public String getSuffix() {
+    public String getSuffix()
+    {
         return "";
     }
 
@@ -101,18 +133,26 @@ public class HandlebarsViewRenderer implements ViewRenderer {
      *
      * @throws IOException If some IO error occurs whilst loading the template.
      */
-    private static Template loadTemplate(String srcUrl) throws IOException {
+    private static Template loadTemplate(String srcUrl) throws IOException
+    {
         return handlebars.compile(srcUrl.replaceAll(".hbs$", ""));
     }
 
-    private Template getTemplate(String srcUrl) throws IOException {
-        if (useCache) {
-            try {
+    private Template getTemplate(String srcUrl) throws IOException
+    {
+        if (useCache)
+        {
+            try
+            {
                 return compilationCache.get(srcUrl);
-            } catch (ExecutionException e) {
+            }
+            catch (ExecutionException e)
+            {
                 throw new IllegalStateException(e);
             }
-        } else {
+        }
+        else
+        {
             return loadTemplate(srcUrl);
         }
     }
